b95910e53997c60c166f76f3c6d52c17e90b4879
knellis <knellis@virtusapolaris.com>
2017-01-09 10:48:15 -0500

    Update bluemix-settings.js

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -51,7 +51,9 @@ var settings = module.exports = {
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
 
-    functionGlobalContext: { },
+    functionGlobalContext: {
+      crcQrujidru('crc')
+    },
 
     storageModule: require("./couchstorage")
 }
